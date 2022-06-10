0ebaab6acacfc40b52b0eec21768aabe83dcf7cc
Shreyas Sarve <i.made.jarvis@gmail.com>
2022-06-10 23:56:27 +0530

    Completed v1.0.0 of extension
     added 2 commands to activate extension and other for adding file/folders
     gitignore
    created separate folder for commands

---
+++ commands/activate.js
@@ -0,0 +1,56 @@
+const vscode = require("vscode");
+const { TextEncoder } = require("util");
+
+const checkForGitinit = async (
+  /** @type {{ uri: vscode.Uri; }} */ currentWorkspaceFolder
+) => {
+  const fs = vscode.workspace.fs;
+  return fs.readDirectory(currentWorkspaceFolder.uri).then((data) => {
+    let exist = false;
+    data.forEach((e) => {
+      if (e[0] == ".git") exist = true;
+    });
+    return exist;
+  });
+};
+const commandCallback = async function () {
+  const currentWorkspaceFolder = vscode.workspace.workspaceFolders[0];
+  vscode.commands.executeCommand("setContext", "open.when.clicked", true);
+  if (currentWorkspaceFolder == undefined) {
+    vscode.window.showErrorMessage("Open folder in workspace first....");
+  } else {
+    const cur = await checkForGitinit(currentWorkspaceFolder);
+    if (!cur) {
+      vscode.window.showErrorMessage("not a git repository....");
+    } else {
+      vscode.window.showInformationMessage("Namstey World....!");
+      const fs = vscode.workspace.fs;
+      vscode.workspace.findFiles(".gitignore").then((d) => {
+        if (d.length == 0)
+          vscode.window
+            .showErrorMessage(
+              "No .gitignore file Exist in Current repository....",
+              "Create File"
+            )
+            .then((d) => {
+              if (d != undefined) {
+                const content = new TextEncoder().encode("");
+                const path = vscode.Uri.parse(
+                  `${currentWorkspaceFolder.uri.path}/.gitignore`
+                );
+                fs.writeFile(path, content).then(() => {
+                  vscode.window.showInformationMessage(
+                    ".gitignore file created"
+                  );
+                });
+              }
+            });
+      });
+    }
+  }
+};
+
+module.exports = {
+  commandName: "help-me-code.activate",
+  commandCallback,
+};
---
+++ commands/add_to_gitignore.js
@@ -0,0 +1,56 @@
+const vscode = require("vscode");
+const { TextEncoder } = require("util");
+
+const addTogitignore = (
+  /** @type {vscode.Uri} */ gitignore,
+  /** @type {string} */ content,
+  /** @type {string} */ pathName
+) => {
+  const fs = vscode.workspace.fs;
+  const newFileContent = content + `\n${pathName}`;
+  const encode = new TextEncoder().encode(newFileContent);
+  fs.writeFile(gitignore, encode).then(() => {
+    vscode.window
+      .showInformationMessage(`${pathName} added to gitignore`, "Undo")
+      .then((select) => {
+        if (select != undefined) {
+          const oldEncode = new TextEncoder().encode(content);
+          fs.writeFile(gitignore, oldEncode);
+        }
+      });
+  });
+};
+const fetchFileContent = async (/** @type {vscode.Uri} */ gitignore) => {
+  const fs = vscode.workspace.fs;
+  return fs.readFile(gitignore).then((content) => {
+    return content.toString();
+  });
+};
+const checkDoesAlreadyExist = (
+  /** @type {string} */ content,
+  /** @type {string} */ pathName
+) => {
+  let exist = false;
+  const fileContent = content.split("\n");
+  fileContent.forEach((e) => {
+    if (e == pathName) exist = true;
+  });
+  return exist;
+};
+const commandCallback = async (/** @type {vscode.Uri} */ l) => {
+  vscode.window.setStatusBarMessage("added to gitIgnore").dispose();
+  const pathName = "/" + vscode.workspace.asRelativePath(l);
+  const workspaceFolder = vscode.workspace.workspaceFolders[0];
+  const gitignore = vscode.Uri.parse(`${workspaceFolder.uri.path}/.gitignore`);
+  const content = await fetchFileContent(gitignore);
+  const exist = checkDoesAlreadyExist(content, pathName);
+  if (exist) {
+    vscode.window.showErrorMessage("Already added to .gitignore ");
+  } else {
+    addTogitignore(gitignore, content, pathName);
+  }
+};
+module.exports = {
+  commandName: "open.when.clicked",
+  commandCallback,
+};
